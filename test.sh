347213b554660fddd28c7fdc32d39597fc66558d
Fabrice Jammes <fabrice.jammes@in2p3.fr>
2020-02-06 12:01:02 +0100

    upd

---
+++ test.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+# Install kubedb and then, a redis cluster
+
+set -euxo pipefail
+
+DIR=$(cd "$(dirname "$0")"; pwd -P)
+
+NS="demo"
+
+# List cluster nodes
+kubectl exec -it redis-cluster-shard0-0 -n "$NS" -c redis -- redis-cli -c cluster nodes
+
+kubectl exec -it redis-cluster-shard0-0 -n "$NS" -c redis -- redis-cli -c cluster keyslot hello
+
+# TODO finish using https://kubedb.com/docs/v0.13.0-rc.0/guides/redis/clustering/redis-cluster/#data-availability
+kubectl exec -it redis-cluster-shard0-0 -n "$NS" -c redis --  redis-cli -c -h redis-cluster
