84c90d353bfae9e55aaa774ca95f44ce057778b0
Fabrice Jammes <fabrice.jammes@in2p3.fr>
2020-02-07 10:22:29 +0100

    upd

--- test-redis.sh
+++ test-redis.sh
@@ -14,4 +14,5 @@ kubectl exec -it redis-cluster-shard0-0 -n "$NS" -c redis -- redis-cli -c cluste
 kubectl exec -it redis-cluster-shard0-0 -n "$NS" -c redis -- redis-cli -c cluster keyslot hello
 
 # TODO finish using https://kubedb.com/docs/v0.13.0-rc.0/guides/redis/clustering/redis-cluster/#data-availability
-kubectl exec -it redis-cluster-shard0-0 -n "$NS" -c redis --  redis-cli -c -h redis-cluster
+kubectl exec -it redis-cluster-shard0-0 -n "$NS" -c redis -- redis-cli -c -h redis-cluster set hello world
+kubectl exec -it redis-cluster-shard0-0 -n "$NS" -c redis -- redis-cli -c -h redis-cluster get hello
